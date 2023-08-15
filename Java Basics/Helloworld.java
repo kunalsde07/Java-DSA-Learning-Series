eec8729f1597f562966cd70bd8f441f725ceb60d
Kunal Lade <kunalsde07@gmail.com>
2023-08-15 14:51:36 +0530

    Error Added

--- Java Basics/Helloworld.java
+++ Java Basics/Helloworld.java
@@ -3,6 +3,7 @@ public class Helloworld {
     public static void main(String[] args) {
 
         System.out.println("Hello, world !");
+        System.err.println("Program is not working git file is added !");
 
     }
 
